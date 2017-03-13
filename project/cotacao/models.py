9ec6d8a95f274e082c4af776b2bbe004d5c313a5
André Leão <andre.sp.leao@gmail.com>
2017-03-12 21:18:59 -0300

    Modificação no campo dataCriacao e serializer de Pedido.
    O campo dataCriacao era do tipo dateTime, para simplificar
    o sistema ele passou a armazenar apenas a data de criacao de um pedido.

--- project/cotacao/models.py
+++ project/cotacao/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from datetime import date
 
 # Create your models here.
 
@@ -22,7 +23,7 @@ class Produto(models.Model):
 class Pedido(models.Model):
     cliente = models.ForeignKey(Cliente, related_name='cliente')     
     codigo = models.AutoField(primary_key=True)   
-    dataCriacao = models.DateTimeField(default=timezone.now) 
+    dataCriacao = models.DateField(auto_now_add=True)
 
     def __str__(self): 
         return str(self.codigo)
