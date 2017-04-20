2842fe0802b5ee98c4eb24b13f98fa581b17b717
André Leão <andre.sp.leao@gmail.com>
2017-04-19 21:04:06 -0300

    Collect Static

--- project/project/settings.py
+++ project/project/settings.py
@@ -86,6 +86,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # }
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES={}
+
 DATABASES['default'].update(db_from_env)
 
 # Password validation
