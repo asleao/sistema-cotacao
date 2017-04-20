5f971495535298bea4cfc52470b2562d3c7b226d
André Leão <andre.sp.leao@gmail.com>
2017-04-19 21:31:35 -0300

    Erro banco de dados

--- project/project/settings.py
+++ project/project/settings.py
@@ -78,16 +78,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES={}
-
-# DATABASES['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
