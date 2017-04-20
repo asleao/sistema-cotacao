f1722467454b338551058de490f66eae1da3ad51
André Leão <andre.sp.leao@gmail.com>
2017-04-19 21:27:47 -0300

    Algumas alterações

--- project/project/settings.py
+++ project/project/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES={}
 
-DATABASES['default'].update(db_from_env)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
